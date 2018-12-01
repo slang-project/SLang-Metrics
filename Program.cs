73aab5a0ebaa9389f186a0667cbbf50f8a0a5590
BorisAnimal <iguru98@yandex.ru>
2018-12-01 17:30:39 +0300

    Testing added

--- Program.cs
+++ Program.cs
@@ -7,18 +7,42 @@ namespace SLangMetrics
 {
     class Program
     {
+        /*
+         * Use argument /tests in console command.
+         *
+         * Тестом является сабдиректория в директории tests\ корня проэкта. в сабдириктории должны содержаться файлы расширений .res и .test
+         * .res - результат парсинга кода в .test файле. Результат может быть true или false.
+         * .test - входные данные для parser. Может быть как положительным, так и отрицательным тестом.
+         *
+         * Все входные тесты (написанные корректно) должны содержать SUCCED в выходной строке, иначе содержат FAILED.
+         * Название файлов, сабдиректорий может быть произвольным
+         */
+        static bool testing = false; // make true while release!
+
         static void Main(string[] args)
         {
-            if (args.Length < 1)
+            if (testing || Array.Exists<String>(args, s => s.ToLower().Contains("/test")))
             {
-                Console.WriteLine("Usage: <ProgramName> <InputFileName>");
-                Environment.Exit(1);
+                Testing.runTests();
             }
-            FileStream file = new FileStream(args[0], FileMode.Open);
+            else
+            {
+                if (args.Length < 1)
+                {
+                    Console.WriteLine("Usage: <ProgramName> <InputFileName>");
+                    Environment.Exit(1);
+                }
+                Console.WriteLine(parseProgram(args[0]));
+            }
+            Console.Read();
+        }
+
+        public static bool parseProgram(String filePath)
+        {
+            FileStream file = new FileStream(filePath, FileMode.Open);
             Scanner scanner = new Scanner(file);
             Parser parser = new Parser(scanner);
-            Console.WriteLine(parser.Parse());
-            Console.Read();
+            return parser.Parse();
         }
     }
 }
---
+++ Testing.cs
@@ -0,0 +1,60 @@
+﻿using SLangLookaheadScanner;
+using SLangParser;
+using System;
+using System.IO;
+
+namespace SLangMetrics
+{
+    internal class Testing
+    {
+        private static bool checkTestDir(DirectoryInfo dir, String pattern)
+        {
+            FileInfo[] files = dir.GetFiles(pattern);
+            if (files.Length == 1)
+            {
+                return true;
+            }
+            else
+            {
+                if (files.Length == 0)
+                    Console.WriteLine("There are no file {0} in {1}!", pattern, dir.FullName);
+                else
+                    Console.WriteLine("There are too many file {0} in {1}!", pattern, dir.FullName);
+                return false;
+            }
+        }
+
+        public static void runTests()
+        {
+            String resExt = "*.res";
+            String testExt = "*.test";
+            DirectoryInfo dir = new DirectoryInfo("tests\\");
+            try
+            {
+                foreach (DirectoryInfo d in dir.GetDirectories())
+                {
+                    if (checkTestDir(d, resExt) && checkTestDir(d, testExt))
+                    {
+                        // Expected result
+                        String res = File.ReadAllText(d.GetFiles(resExt)[0].FullName).Trim();
+                        // Parsing
+                        bool ret = Program.parseProgram(d.GetFiles(testExt)[0].FullName.Trim());
+                        if (res.ToLower().Equals(ret.ToString().ToLower()))
+                        {
+                            Console.WriteLine("Test {0} SUCCED!", d.Name);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Test {0} FAILED!", d.Name);
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("IOException: " + e.GetBaseException());
+            }
+            Console.WriteLine("---------------- Tests finished ----------------\n\n");
+        }
+    }
+}
