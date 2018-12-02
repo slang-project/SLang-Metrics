bdb6843fc6925496c4aafa0b4af3ef96fc2c28a6
Denis Chernikov <d.chernikov@innopolis.ru>
2018-12-02 22:51:58 +0300

    Module access mode changed

--- src/MetricCollector.cs
+++ src/MetricCollector.cs
@@ -4,7 +4,7 @@ namespace Metrics
 {
     class MetricCollector
     {
-        Module parsedModule;
+        private Module parsedModule;
 
         internal MetricCollector(string fileName)
         {
