260bc9e04b93da6f77c0878f71367d72ab84d0a5
Denis Chernikov <d.chernikov@innopolis.ru>
2018-12-03 00:57:05 +0300

    Minor codestyle changes

--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,6 @@ namespace SLangMetrics
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             if (args.Length < 1)
@@ -13,15 +12,15 @@ namespace SLangMetrics
                 Console.WriteLine("Usage: <ProgramName> <InputFileName> OR <ProgramName> /test");
                 Environment.Exit(1);
             }
+
             if (Testing.isTestingMode || Array.Exists<String>(args, s => s.ToLower().Contains("/test")))
             {
                 Testing.runTests();
+                return;
             }
-            else
-            {
-                MetricCollector coll = new MetricCollector(args[0]);
-                // TODO call representation of metrics (toHTML or CliInterface)
-            }
+
+            MetricCollector coll = new MetricCollector(args[0]);
+            // TODO call representation of metrics (toHTML or CliInterface)
         }
     }
 }
